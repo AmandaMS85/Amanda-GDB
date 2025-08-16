7a57c3e82ed95cd5d3928a7a9964cb7597527309
Amanda Martins Soares <amanda_soares85@hotmail.com>
2025-08-15 23:14:28 -0300

    Add files via upload

---
+++ mimo15082025.py
@@ -0,0 +1,22 @@
+name = input("Hello what's your name? ")
+
+print(f"Nice to meet you, {name}")
+
+age= 40
+bot_age = 3
+age_difference = age - bot_age
+
+print("How old are you?") 
+print(age)
+
+print(f"Wow, I Have {bot_age}" )
+
+print(f"You have {age_difference} than me!")
+
+color = input("What's your favorite color? ")
+
+pc_color = "pink"
+
+print(f"Cool, my favorite color is {pc_color}")
+
+
